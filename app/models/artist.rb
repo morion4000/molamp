0eda539d151fde968464da385d36748f90eac77f
Ionut Moraru <morion4000@gmail.com>
2012-09-23 16:13:09 +0300

    downcase for cache keys

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,19 +7,19 @@ class Artist
   end
   
   def info
-    Rails.cache.fetch("/artists/#{@name}/info", :expires_in => 7.days, :compress => true) do
+    Rails.cache.fetch("/artists/#{@name.downcase!}/info", :expires_in => 7.days, :compress => true) do
       @lastfm.artist.get_info(@name)
     end
   end
   
   def top_tracks
-    Rails.cache.fetch("/artists/#{@name}/top_tracks", :expires_in => 7.days, :compress => true) do
+    Rails.cache.fetch("/artists/#{@name.downcase!}/top_tracks", :expires_in => 7.days, :compress => true) do
       @lastfm.artist.get_top_tracks(:artist => @name, :limit => 30)
     end
   end
   
   def top_albums
-    Rails.cache.fetch("/artists/#{@name}/top_albums", :expires_in => 7.days, :compress => true) do
+    Rails.cache.fetch("/artists/#{@name.downcase!}/top_albums", :expires_in => 7.days, :compress => true) do
       @lastfm.artist.get_top_albums(:artist => @name, :limit => 10)
     end
   end
@@ -29,7 +29,7 @@ class Artist
       name = @name  
     end
     
-    Rails.cache.fetch("/artists/#{@name}/search", :expires_in => 7.days, :compress => true) do
+    Rails.cache.fetch("/artists/#{@name.downcase!}/search", :expires_in => 7.days, :compress => true) do
       @lastfm.artist.search(:artist => @name)
     end
   end
