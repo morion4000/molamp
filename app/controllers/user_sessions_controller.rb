751be5ff054f33d06a19f009f28755032b3f13af
Ionut Moraru <morion4000@gmail.com>
2012-11-05 23:56:20 +0200

    go to account/social after sign up and login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,8 +9,8 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
-      redirect_back_or_default root_path
+      flash[:notice] = 'Login successful!'
+      redirect_to '/account/social'
     else
       render :action => :new
     end
@@ -18,7 +18,7 @@ class UserSessionsController < ApplicationController
   
   def destroy
     current_user_session.destroy
-    flash[:notice] = "Logout successful!"
+    flash[:notice] = 'Logout successful!'
     redirect_back_or_default new_user_session_url
   end
 end
