94c450500451ec3a46a074a1449418369a71f207
Ionut Moraru <morion4000@gmail.com>
2012-11-06 17:18:02 +0200

    reverted the db config

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,6 @@
 ActiveRecord::Schema.define(:version => 20121101175443) do
 
   create_table "users", :force => true do |t|
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
     t.string   "email",                                 :null => false
     t.string   "crypted_password",                      :null => false
     t.string   "password_salt",                         :null => false
@@ -22,11 +20,14 @@ ActiveRecord::Schema.define(:version => 20121101175443) do
     t.string   "single_access_token",                   :null => false
     t.string   "perishable_token",                      :null => false
     t.integer  "login_count",         :default => 0,    :null => false
+    t.integer  "failed_login_count",  :default => 0,    :null => false
     t.datetime "last_request_at"
-    t.datetime "last_login_at"
     t.datetime "current_login_at"
-    t.string   "last_login_ip"
+    t.datetime "last_login_at"
     t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "facebook_token"
     t.string   "lastfm_token"
     t.string   "lastfm_username"
