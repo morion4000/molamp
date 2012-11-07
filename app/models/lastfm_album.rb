987270db0f8334bb8a958f99310f4266af9b715a
Ionut Moraru <morion4000@gmail.com>
2012-11-07 19:16:42 +0200

    forgot about this file

--- app/models/lastfm_album.rb
+++ app/models/lastfm_album.rb
@@ -1,13 +1,28 @@
-class Album 
-  attr_accessor :name, :artist, :lastfm
+class LastfmAlbum < Album
+  attr_accessor :lastfm
   
   def initialize(name, artist, lastfm)
+    @lastfm = lastfm
+    
     @name = name
     @artist = artist
-    @lastfm = lastfm
+    
+    info = self.get_info
+    
+    @id = info['id']
+    @mbid = info['mbid']
+    @name = info['name']
+    @artist = info['artist']
+    @url = info['url']
+    @releasedate = info['releasedate']
+    @image = info['image']
+    @listeners = info['listeners']
+    @playcount = info['playcount']
+    @toptags = info['toptags']
+    @tracks = info['tracks']
   end
   
-  def info
+  def get_info
     Rails.cache.fetch("/albums/#{@artist}/#{@name}#info", :expires_in => 7.days, :compress => true) do
       @lastfm.album.get_info(:artist => @artist, :album => @name)
     end
