fb85283b55029a212fdbd0fb12095597f4144f15
Ionut Moraru <morion4000@gmail.com>
2012-11-11 01:33:27 +0200

    ....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,7 +83,7 @@ class ApplicationController < ActionController::Base
     if !code.to_s.blank? and return_to.to_s.blank? 
       # Auth referral
         query = Rack::Utils.parse_nested_query(request.query_string)
-        query.delete('code')
+        #query.delete('code')
         query_string = Rack::Utils.build_query(query)
                   
         redirect_to '/auth/facebook?return_to=' +
