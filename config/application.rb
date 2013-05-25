0258682979f4bc1d5cbd3dc88bf48723f992293f
morion4000 <morion4000@gmail.com>
2013-05-25 16:05:25 +0300

    arghh

--- config/application.rb
+++ config/application.rb
@@ -57,10 +57,8 @@ module Molamp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2'
+    config.assets.version = '1.3'
 
     config.assets.initialize_on_precompile = false
-
-    config.assets.precompile += ['pages']
   end
 end
